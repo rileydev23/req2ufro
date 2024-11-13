954fad3563ccec5a9c1b976332d622f72cab0b0f
Riley <62442550+rileydev23@users.noreply.github.com>
2024-11-13 00:33:11 -0300

    Update environment.js

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
 const TOKEN_SECRET = process.env.TOKEN_SECRET;
 
